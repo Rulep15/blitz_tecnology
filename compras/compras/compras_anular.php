dcfe3080bbba3354a4a4f895575cb900bf36e3d3
Rulep15 <lucasrulep@gmail.com>
2022-11-06 20:49:06 -0300

    blitz

--- compras/compras/compras_anular.php
+++ compras/compras/compras_anular.php
@@ -12,7 +12,7 @@
 </HEAD>
 
 <BODY class="hold-transition skin-purple sidebar-mini">
-    <div class="wrapper" style="background-color: #1E282C;">
+    <div id="wrapper">
         <?php require '../../estilos/cabecera.ctp'; ?>
         <?php require '../../estilos/izquierda.ctp'; ?>
         <div class="content-wrapper" style="background-color: #BBBBBB;">
@@ -22,9 +22,9 @@
                         <div class="box box-primary">
                             <div class="box-header">
                                 <i class="ion ion-edit"></i>
-                                <h3 class="box-title">Confirmar Compra</h3>
+                                <h3 class="box-title">Anular Compra</h3>
                                 <div class="box-tools">
-                                    <a href="compras_index.php" class="btn btn-primary pull-right btn-sm">
+                                    <a href="pedidosc_index.php" class="btn btn-primary pull-right btn-sm">
                                         <i class="fa fa-arrow-left"></i>
                                     </a>
                                 </div>
@@ -33,7 +33,8 @@
                                 <div class="box-body">
                                     <?php $resultado = consultas::get_datos("SELECT * FROM v_compras WHERE id_compra =" . $_GET['vidcompra']); ?>
                                     <div class="form-group-lg form-group-sm">
-                                        <input class="form-control" type="hidden" name="voperacion" value="2">
+                                        <input class="form-control" type="hidden" name="voperacion" value="3">
+
                                         <div class="form-group">
                                             <label class="col-lg-2 control-label">Codigo de Compra</label>
                                             <div class="col-lg-8">
@@ -61,11 +62,10 @@
                                                 <input class="form-control" type="text" name="vnrofactura" readonly="" value="<?php echo $resultado[0]['nro_factura']; ?>">
                                             </div>
                                         </div>
-
                                     </div>
-                                </div>
-                                <div class="box-footer" style="text-align: right;">
-                                    <button class="btn btn-success" type="submit">Confirmar</button>
+                                    <div class="box-footer" style="text-align: right;">
+                                        <button class="btn btn-danger" type="submit">Anular</button>
+                                    </div>
                                 </div>
                             </form>
                         </div>
