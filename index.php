824eaf4bc42fdb4e0b073cd090de46a7bd8b669c
Rulep15 <lucasrulep@gmail.com>
2022-11-06 14:07:24 -0300

    Acesso al Sistema

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ($_SESSION) {
     <head>
         <meta charset="UTF-8">
         <meta content="width=devicewidth, initial-scale=1, maximum-scalable=no"name="viewport">
-        <title>Acceso a Sistema!</title>
+        <title>Acceso al Sistema</title>
         <?php require 'estilos/css_lte.ctp'; ?>
         <style>
             body{
@@ -64,7 +64,7 @@ if ($_SESSION) {
                     <input type="password" name="pass" class="form-control" placeholder="Ingrese contraseña" required="">
                     <span class="glyphicon glyphicon-qrcode form-control-feedback"></span>
                 </div>
-                <button class="btn btn-lg btn-info btn-block" type="submit" style="background-color: #0008CBA; padding: 5px 5px;">Iniciar Sesion</button>
+                <button class="btn btn-lg btn-info btn-block" type="submit" style="background-color: #0008CBA; padding:5px 5px;">Iniciar Sesion</button>
                 <div class="checkbox" style="padding: 0px 20px;">
                     <p style="text-align: center;"class="help-block"><a href="#">¿No puede Ingresar a su cuenta?</a></p>
                 </div>
