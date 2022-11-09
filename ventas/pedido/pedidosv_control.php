0ef17e74b397fd6e425966e49f59052f67927cbe
Rulep15 <lucasrulep@gmail.com>
2022-11-09 17:04:52 -0300

    blitz

--- ventas/pedido/pedidosv_control.php
+++ ventas/pedido/pedidosv_control.php
@@ -5,24 +5,27 @@ session_start();
 
 $operacion = $_REQUEST['voperacion'];
 $codigo = $_REQUEST['vidpedido'];
-$fecha = $_REQUEST['vfecha'];
 $usuario = $_REQUEST['vusuario'];
-$observacion = $_REQUEST['vobservacion'];
+$fecha = $_REQUEST['vfecha'];
+$total = $_REQUEST['vtotal'];
+$cliente = $_REQUEST['vidcliente'];
 $estado = $_REQUEST['vestado'];
 
 
-$sql = "SELECT sp_compras_pedidos(" . $operacion . "," .
-        (!empty($codigo) ? $codigo : 0) . "," .
-        (!empty($usuario) ? $usuario : 0) . ",'" .
-        (!empty($fecha) ? $fecha : "01-01-0001") . "','" .
-        (!empty($observacion) ? $observacion : "VACIO") . "','" .
-        (!empty($estado) ? $estado : "VACIO") . "') AS pedidosc;";
+
+$sql = "SELECT sp_ventas_pedidos(" . $operacion . "," .
+    (!empty($codigo) ? $codigo : 0) . "," .
+    (!empty($usuario) ? $usuario : 0) . ",'" .
+    (!empty($fecha) ? $fecha : "01-01-0001") . "'," .
+    (!empty($total) ? $total : 0) . "," .
+    (!empty($cliente) ? $cliente : 0) . ",'" .
+    (!empty($estado) ? $estado : "VACIO") . "') AS pedidosv;";
 $resultado = consultas::get_datos($sql);
 
-if ($resultado[0]['pedidosc'] != NULL) {
-    $_SESSION['mensaje'] = $resultado[0]['pedidosc'];
-    header("location:pedidosc_index.php");
+if ($resultado[0]['pedidosv'] != NULL) {
+    $_SESSION['mensaje'] = $resultado[0]['pedidosv'];
+    header("location:pedidosv_index.php");
 } else {
     $_SESSION['mensaje'] = 'Error:' . $sql;
-    header("location:pedidosc_index.php");
+    header("location:pedidosv_index.php");
 }
