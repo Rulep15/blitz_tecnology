090d6e7f5cc21cb2c3938d2ead6f1629abad9bdf
Rulep15 <lucasrulep@gmail.com>
2022-11-09 23:39:27 -0300

    blitz

--- ventas/ventas/ventas_control.php
+++ ventas/ventas/ventas_control.php
@@ -3,14 +3,14 @@
 require '../../conexion.php';
 session_start();
 
-$operacion = $_REQUEST['voperacion'];
-$codigo = $_REQUEST['vidcompra'];
-$usuario = $_SESSION['usu_cod'];
-$proveedor = $_REQUEST['vidproveedor'];
+$operacion = $_REQUEST['operacion'];
+$codigo = $_REQUEST['vidventa'];
+$usuario = $_SESSION['vusuario'];
+$cliente = $_REQUEST['vidcliente'];
 $nrofactura = $_REQUEST['vnrofactura'];
 $fecha = $_REQUEST['vfecha'];
 $condicion = $_REQUEST['vcondicion'];
-$canticuo = $_REQUEST['vcantidadcuota'];
+$canticuo = $_REQUEST['vcanticuo'];
 $intervalo = $_REQUEST['vintervalo'];
 
 $iva = 0;
@@ -19,25 +19,25 @@ $estado = "ACTIVO";
 
 
 
-$sql = "SELECT sp_compras(" . $operacion . "," .
-        (!empty($codigo) ? $codigo : 0) . "," .
-        (!empty($usuario) ? $usuario : 0) . "," .
-        (!empty($proveedor) ? $proveedor : 0) . ",'" .
-        (!empty($nrofactura) ? $nrofactura : "VACIO") . "','" .
-        (!empty($fecha) ? $fecha : "01-01-0001") . "','" .
-        (!empty($condicion) ? $condicion : "VACIO") . "'," .
-        (!empty($canticuo) ? $canticuo : 0) . "," .
-        (!empty($intervalo) ? $intervalo : 0) . "," .
-        (!empty($iva) ? $iva : 0) . "," .
-        (!empty($total) ? $total : 0) . ",'" .
-        (!empty($estado) ? $estado : "VACIO") . "') AS compras;";
+$sql = "SELECT sp_ventas(" . $operacion . "," .
+    (!empty($codigo) ? $codigo : 0) . "," .
+    (!empty($usuario) ? $usuario : 0) . "," .
+    (!empty($cliente) ? $cliente : 0) . ",'" .
+    (!empty($nrofactura) ? $nrofactura : "VACIO") . "','" .
+    (!empty($fecha) ? $fecha : "01-01-0001") . "','" .
+    (!empty($condicion) ? $condicion : "VACIO") . "'," .
+    (!empty($canticuo) ? $canticuo : 0) . "," .
+    (!empty($intervalo) ? $intervalo : 0) . "," .
+    (!empty($iva) ? $iva : 0) . "," .
+    (!empty($total) ? $total : 0) . ",'" .
+    (!empty($estado) ? $estado : "VACIO") . "') AS ventas;";
 $resultado = consultas::get_datos($sql);
 
-if ($resultado[0]['compras'] != NULL) {
-    $valor = explode("*" , $resultado[0]['compras']);
+if ($resultado[0]['ventas'] != NULL) {
+    $valor = explode("*", $resultado[0]['ventas']);
     $_SESSION['mensaje'] = $valor[0];
-    header("location:". $valor[1].".php");
+    header("location:" . $valor[1] . ".php");
 } else {
     $_SESSION['mensaje'] = 'Error:' . $sql;
-    header("location:compras_index.php");
+    header("location:ventas_index.php");
 }
