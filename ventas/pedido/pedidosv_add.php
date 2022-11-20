fbed756bd04e6806214a4511553c374075ccc1dc
Rulep15 <lucasrulep@gmail.com>
2022-11-20 20:24:20 -0300

    blitz

---
+++ ventas/pedido/pedidosv_add.php
@@ -0,0 +1,126 @@
+<?php session_start(); ?>
+<!DOCTYPE>
+<HTML>
+
+<HEAD>
+    <meta charset="utf-8">
+    <meta content="width=devicewidth, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <?php
+    include '../../conexion.php';
+    require '../../estilos/css_lte.ctp';
+    ?>
+</HEAD>
+
+<BODY class="hold-transition skin-purple sidebar-mini">
+    <div id="wrapper" style="background-color: #1E282C;">
+        <?php require '../../estilos/cabecera.ctp'; ?>
+        <?php require '../../estilos/izquierda.ctp'; ?>
+        <div class="content-wrapper" style="background-color: #333333;">
+            <div class="content">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-xs-12">
+                        <?php if (!empty($_SESSION['mensaje'])) { ?>
+                            <?php
+                            $mensaje = explode("_/_", $_SESSION['mensaje']);
+                            if (($mensaje[0] == 'NOTICIA')) {
+                                $class = "success";
+                            } else {
+                                $class = "danger";
+                            }
+                            ?>
+                            <div class="alert alert-<?= $class; ?>" role="alert" id="mensaje">
+                                <i class="ion ion-information-circled"></i>
+                                <?php
+                                echo $mensaje[1];
+                                $_SESSION['mensaje'] = '';
+                                ?>
+                            </div>
+                        <?php } ?>
+                        <div class="box box-primary">
+                            <div class="box-header">
+                                <i class="ion ion-plus"></i>
+                                <h3 class="box-title">Agregar Pedido</h3>
+                                <div class="box-tools">
+                                    <a href="pedidosv_index.php" class="btn btn-primary pull-right btn-sm">
+                                        <i class="fa fa-arrow-left"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <form action="pedidosv_control.php" method="POST" accept-charset="UTF-8" class="form-horizontal">
+                                <div class="box-body">
+                                    <div class="row">
+                                        <input type="hidden" name="operacion" value="1">
+                                        <input type="hidden" name="vestado" value="ACTIVO" />
+                                        <div class="form-group">
+                                            <label class="control-label col-lg-2 col-sm-2 col-xs-4">Codigo de Pedido</label>
+                                            <?php $cc = consultas::get_datos("SELECT COALESCE(MAX(id_pedido),0)+1 AS ultimo FROM ventas_pedidos") ?>
+                                            <div class="col-lg-6 col-sm-6 col-xs-7">
+                                                <input class="form-control" type="text" name="vidpedido" readonly="" value="<?php echo $cc[0]['ultimo']; ?>">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="control-label col-lg-2 col-sm-2 col-xs-4">Fecha</label>
+                                            <div class="col-lg-6 col-sm-6 col-xs-7">
+                                                <input class="form-control" type="text" name="vfecha" readonly="" value="<?php echo date("d-m-Y"); ?>">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="control-label col-lg-2 col-sm-2 col-xs-4">Usuario</label>
+                                            <div class="col-lg-6 col-sm-6 col-xs-7">
+                                                <input class="form-control" type="hidden" name="vusuario" value="<?php echo $_SESSION['usu_cod']; ?>" />
+                                                <input class="form-control" type="text" name="vusunick" readonly="" value="<?php echo $_SESSION['usu_nick']; ?>">
+                                            </div>
+                                        </div>
+                                       
+                                        <!--Cliente-->
+                                        <div class="form-group">
+                                            <label class="control-label col-lg-2 col-sm-2 col-xs-2">Cliente</label>
+                                            <div class="col-lg-6 col-sm-6 col-xs-6">
+                                                <div class="input-group">
+                                                    <?php $cliente = consultas::get_datos("SELECT * FROM ref_cliente ORDER BY id_cliente"); ?>
+                                                    <select class="select2" name="vidcliente" required="" style="width: 500px;">
+                                                        <?php
+                                                        if (!empty($cliente)) {
+                                                            foreach ($cliente as $c) {
+                                                        ?>
+                                                                <option value="<?php echo $c['id_cliente']; ?>"><?php echo $c['nombres']; ?></option>
+                                                            <?php
+                                                            }
+                                                        } else {
+                                                            ?>
+                                                            <option value="">Debe seleccionar al menos un cliente</option>
+                                                        <?php }
+                                                        ?>
+                                                    </select>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-lg-6 col-sm-6 col-xs-7">
+                                                <input class="form-control" type="hidden" name="vestado" readonly="" value="<?php echo $c['estado']; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-footer" style="text-align: right;">
+                                    <button class="fa fa-save btn btn-success" type="submit"> Guardar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php require '../../estilos/pie.ctp'; ?>
+</BODY>
+<?php require '../../estilos/js_lte.ctp'; ?>
+<script>
+    /*MENSAJE DE INSERT MARCAS, TIPO,. ETC*/
+    $("#mensaje").delay(1000).slideUp(200, function() {
+        $(this).alert('close')
+    });
+</script>
+
+</HTML>
